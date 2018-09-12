7cf30a6dd8117574eea18a66a8892c13b85c6065
alexandrbektashev <alexandr.bektashev@gmail.com>
2018-09-12 16:59:16 +0300

    empty project

---
+++ DS_guard1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_guard1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
